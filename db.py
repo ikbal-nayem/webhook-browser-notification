59b97c0d1481074c443a51b5cbe26452c3cc2346
Ikbal <ikbalnayem000@gmail.com>
2024-07-12 21:03:14 +0600

    rename

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import firestore, credentials
 
-cred = credentials.Certificate("firebase.json")
+cred = credentials.Certificate("firebase.conf.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
